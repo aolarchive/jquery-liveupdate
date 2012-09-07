1ce471071229f76e50ef99a6c69a2305a5c616b5
Nate Eagle <nate@nateeagle.com>
2012-09-07 14:08:28 -0400

    added logic to api plugin to deal with updates, changes, and deletes

--- src/jquery.liveblogapi.js
+++ src/jquery.liveblogapi.js
@@ -12,9 +12,11 @@
 
             liveBlogApi = function (customOptions) {
 
-                var lastUpdate = 0,
+                var state = {},
+                    lastUpdate = 0,
                     count = 0,
                     options = $.extend(true, {}, defaultOptions, customOptions),
+
                     fetch = function () {
                         // Fetch data from API
                         var callback = options.callbackPrefix + count,
@@ -38,10 +40,72 @@
                                 //count += 1;
 
                                 if (response.data) {
+
+                                    updateState(response.data);
+                                    console.log('State:', state);
+
+                                    //$this.trigger('update', state);
                                     $this.trigger('update', response.data);
                                 }
                             }
                         });
+                    },
+
+                    alterState = function (data, difference) {
+                        var types = { 1: "text", 2: "image", 4: "comment" };
+
+                        for (i = 0, length = data.length; i < length; i += 1) {
+                            update = data[i];
+
+                            // Normalize the property names
+                            normalizedUpdate = {
+                                memberId: update.m,
+                                date: new Date(update.t * 1000),
+                                difference: difference,
+                                type: types[update.type] || 'unknown',
+                                content: update.d,
+                                tags: update.md && update.md.tags || []
+                            };
+
+                            state[update.id] = normalizedUpdate;
+                        }
+                    },
+
+                    updateState = function (data) {
+                        var i, length, item, deletedItem, update, normalizedUpdate;
+
+                        // Reset the difference labels
+                        for (item in state) {
+                            if (state.hasOwnProperty(item)) {
+                                // If the item has been marked for deletion, delete it
+                                if (state[item].difference && state[item].difference === 'deleted') {
+                                    delete state[item];
+                                } else {
+                                    delete state[item].difference;
+                                }
+                            }
+                        }
+
+                        // Add new updates
+                        if (data.updates && $.isArray(data.updates)) {
+                            alterState(data.updates, 'new');
+                        }
+
+                        // Make changes
+                        if (data.changes && $.isArray(data.changes)) {
+                            alterState(data.changes, 'changed');
+                        }
+
+                        // Delete updates
+                        if (data.deletes && $.isArray(data.deletes)) {
+                            for (i = 0, length = data.deletes.length; i < length; i += 1) {
+                                deletedItem = data.deletes[i].id;
+
+                                if (state.hasOwnProperty(deletedItem)) {
+                                    state[deletedItem].difference = 'deleted';
+                                }
+                            }
+                        }
                     };
 
                 fetch();
