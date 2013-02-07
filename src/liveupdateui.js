d00cc77a7152a26d6f46437f0ff722b911b0b9ef
Jeremy Jannotta <jeremy.jannotta@teamaol.com>
2013-02-07 16:46:22 -0500

    * Change featured blogger styles, to make profile image and name smaller, and shift the name to the right of the image

--- src/liveupdateui.js
+++ src/liveupdateui.js
@@ -310,7 +310,7 @@
                     $profileImage = $('<img/>', {
                       'class': 'lb-profile-image',
                       src: memberSettings.profileImage,
-                      alt: item.memberName,
+                      alt: '',
                       title: item.memberName
                     }).appendTo($postAuthorTab);
                   }
